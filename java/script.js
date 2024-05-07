3485a77618fd67044aa6c150d555b21cc3dc4330
DataKvinikadze <datakvinikadze07@gmail.com>
2024-05-07 15:45:37 +0400

    adding counting taps

--- java/script.js
+++ java/script.js
@@ -3,8 +3,9 @@
 let input = document.getElementById("input");
 let btn = document.getElementById("main_btn");
 let unorderedList = document.querySelector(".list");
-let x = 1;
-let header = document.querySelector("h1");
+let x = 0;
+let amount = document.querySelector("h1");
+amount.textContent = `Task to do: ${x}`;
 
 // Adding EventListener function:
 
@@ -35,17 +36,38 @@ btn.addEventListener("click", function () {
   if (input.value.trim() !== "") {
     listContent.textContent = input.value.trim();
     unorderedList.appendChild(list_item);
+    x++;
+    console.log(x);
+    amount.textContent = `Task to do: ${x}`;
   }
 
   // done_delete Event Listener
 
   btn2.addEventListener("click", function () {
     list_item.remove();
+
+    // Counter
+    x--;
+    console.log(x);
+    amount.textContent = `Task to do: ${x}`;
   });
 
   btn1.addEventListener("click", function () {
     listContent.classList.toggle("done");
+
+    // counter
+    if (listContent.classList.contains("done")) {
+      x--;
+      console.log(x);
+      amount.textContent = `Task to do: ${x}`;
+    } else {
+      x++;
+      console.log(x);
+      amount.textContent = `Task to do: ${x}`;
+    }
   });
+
+  // Erti ragac ver gavige, magalitad roca vawer shesrulebis gilaks da shemdgom nagvis gilaks amount akldeba 2jer, kodi mase mushaobs da ise rogor unda davwero ro magalitad tu ukve shesrulebulzea dawerili da shemdeg davawert nagavs mashin 1 daematos da ara zedmeti 1 daakldes.
 
   input.value = "";
 });
